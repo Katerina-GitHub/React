bf891569131c67781e33046ff264264d05d37ddf
Kate <katia230679@mail.ru>
2022-03-25 15:28:20 +0300

    My hw

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./index.css";
 
-function App() {
+export const MessageList = () => {
+  const [value, setValue] = useState("");
+  const [messages, setMessages] = useState([
+    {
+      author: "Bot",
+      text: "its my message",
+      date: new Date().toLocaleDateString(),
+    },
+  ]);
+  const sendMessage = () => {
+    if (value) {
+      setMessages([
+        ...messages,
+        {
+          author: "User",
+          message: value,
+          date: new Date().toLocaleDateString(),
+        },
+      ]);
+      setValue("");
+    }
+  };
+  useEffect(() => {
+    const lastMessages = messages[messages.length - 1];
+    let timerId = null;
+
+    if (messages.length && lastMessages.author === "User") {
+      timerId = setTimeout(() => {
+        setMessages([
+          ...messages,
+          {
+            author: "Bot",
+            message: "Hi, this is Bot answer",
+            date: new Date().toLocaleDateString(),
+          },
+        ]);
+      }, 500);
+    }
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [messages]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <input
+          placeholder="Введите сообщение здесь"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button value onClick={sendMessage}>
+          Отправить
+        </button>
+        {messages.map((msg) => (
+          <div>
+            <h1>{msg.author}</h1>
+            <h3>{msg.text}</h3>
+            <h5>{msg.date}</h5>
+          </div>
+        ))}
+      </div>
+    </>
   );
-}
-
-export default App;
+};
