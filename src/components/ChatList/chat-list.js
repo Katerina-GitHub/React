20f9a7dbed343f9877d717a613c4b11a65474667
Kate <katia230679@mail.ru>
2022-04-11 16:27:58 +0300

    hwlesson6

--- src/components/ChatList/chat-list.js
+++ src/components/ChatList/chat-list.js
@@ -1,24 +1,46 @@
-import { useState } from "react";
-import { Link, useParams } from "react-router-dom";
-import { List } from "@mui/material";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { List, Button } from "@mui/material";
 import { Chat } from "./chat";
+import {
+  createConversation,
+  deleteConversation,
+} from "../../store/conversations";
+
+const selector = (state) => {
+  return state.conversations.conversations;
+};
 
 export function ChatList() {
-  const { roomId } = useParams;
-  const [chats] = useState([
-    "room1",
-    "room2",
-    "room3",
-    "room4",
-    "room5",
-    "room6",
-  ]);
+  const { roomId } = useParams();
+  const dispatch = useDispatch();
+  const navigate = useNavigate("/chat");
+  const conversations = useSelector(selector);
+  const create = () => {
+    const name = prompt("Введите комнату");
 
+    const isValidName = !conversations.includes(name);
+    if (!!name && isValidName) {
+      dispatch(createConversation(name));
+    } else {
+      alert("Такая комната уже есть");
+    }
+  };
+  const deleteConv = (conversation) => {
+    dispatch(deleteConversation(conversation));
+    setTimeout(() => navigate("/chat"));
+  };
   return (
     <List component="nav">
-      {chats.map((chat, index) => (
+      <Button color="info" onClick={create}>
+        Новая комната
+      </Button>
+      {conversations.map((chat, index) => (
         <Link key={index} to={`/chat/${chat}`}>
           <Chat title={chat} selected={roomId === chat} />
+          <Button color="info" onClick={() => deleteConv(chat)}>
+            Удалить комнату
+          </Button>
         </Link>
       ))}
     </List>
